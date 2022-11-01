8cd540ae2e26a943e36a88ad51a6fae5c88d6d82
Daniel Freire <daniel.carneiro.freire@gmail.com>
2022-11-01 18:17:13 -0300

    Added progress bar, and better command line

--- src/intruder.rs
+++ src/intruder.rs
@@ -1,7 +1,6 @@
 use super::request_template::RequestTemplate;
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 
-use futures::stream::FuturesUnordered;
 use futures::{stream, StreamExt};
 
 use hyper::client::HttpConnector;
@@ -11,16 +10,20 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
+use indicatif::ProgressBar;
+
 pub(crate) struct Intruder {
     pub(crate) client: Client<HttpConnector>,
     pub(crate) req_templ: RequestTemplate,
+    concurrent_reqs: usize
 }
 
 impl Intruder {
-    pub(crate) fn new(req_file: File) -> Result<Self> {
+    pub(crate) fn new(req_file: File, concurrent_reqs: usize) -> Result<Self> {
         Ok(Intruder {
             client: Client::new(),
             req_templ: RequestTemplate::from_file(req_file)?,
+            concurrent_reqs
         })
     }
 
@@ -28,7 +31,10 @@ impl Intruder {
         &self,
         req: (Request<Body>, String),
     ) -> Result<(Response<Body>, String)> {
-        let resp = self.client.request(req.0).await?;
+        let resp = match self.client.request(req.0).await.context(req.1.clone()) {
+            Ok(out) => out,
+            Err(_) => return Err(anyhow!(req.1)),
+        };
         Ok((resp, req.1))
     }
 
@@ -44,12 +50,52 @@ impl Intruder {
         for req in reqs {
             futures.push(self.send_req(req))
         }
-        let mut futures = stream::iter(futures).buffer_unordered(5);
-        while let Some(resp) = futures.next().await {
-            let (resp, pw) = resp?;
-            if resp.status() == 200 {
-                return Ok(pw);
+
+        let bar = ProgressBar::new(futures.len() as u64);
+
+        let mut conc = self.concurrent_reqs;
+        let mut futures = stream::iter(futures).buffer_unordered(conc);
+        let mut errors: Vec<String> = vec![];
+
+        // do while errors is not empty
+        while {
+            while let Some(res) = futures.next().await {
+                let (resp, pw);
+                match res {
+                    Ok(result) => {
+                        bar.inc(1);
+                        (resp, pw) = result;
+                    }
+                    Err(err) => {
+                        errors.push(err.to_string());
+                        continue;
+                        //
+                    }
+                };
+                if resp.status() == 200 {
+                    return Ok(pw);
+                }
             }
+            !errors.is_empty()
+        } {
+            // If errors is not empty either the request is malformed or you are requesting too fast
+            if bar.position() == 0 {
+                return Err(anyhow!("Your requests are not getting through"));
+            }
+            let mut new_futures = vec![];
+            let reqs = errors
+                .into_iter()
+                .map(|pw| self.req_templ.replace_then_request(pw))
+                .filter_map(|req| req.ok());
+            for req in reqs {
+                new_futures.push(self.send_req(req))
+            }
+            conc = conc/2;
+            if conc == 0 {
+                break;
+            }
+            futures = stream::iter(new_futures).buffer_unordered(conc);
+            errors = vec![];
         }
 
         Err(anyhow!("Password not found"))
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-use anyhow::Result;
-use std::env::args;
-use std::fs::File;
 use crate::intruder::Intruder;
+use anyhow::Result;
+use std::fs::File;
 mod request_template;
-
+use clap::Parser;
 mod intruder;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to request template file
+    #[arg(index = 1, value_hint = clap::ValueHint::FilePath)]
+    req_f: std::path::PathBuf,
+
+    /// Path to password file
+    #[arg(index = 2, value_hint = clap::ValueHint::FilePath)]
+    pass_f: std::path::PathBuf,
+
+    /// Number of concurrent requests
+    #[arg(short, long, default_value_t = 1)]
+    concurrent_requests: usize,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args: Vec<String> = args().skip(1).collect();
-    if args.len() != 2 {
-        std::process::exit(1);
-    }
+    let args = Args::parse();
 
-    let pass_file = File::open(&args[1])?;
-    let intruder = Intruder::new(File::open(&args[0])?)?;
+    let intruder = Intruder::new(File::open(&args.req_f)?, args.concurrent_requests)?;
 
+    let pass_file = File::open(&args.pass_f)?;
     let pw = intruder.bruteforce(pass_file).await?;
     println!("{:}", pw);
 
